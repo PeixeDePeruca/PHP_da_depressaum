7b2cc9379b73ed1e255ff4d01c26b58e03f2fc4e
Anchova <anchovaluiz@gmail.com>
2026-07-30 18:33:10 -0300

    questão 6 - atividade 1

---
+++ Atividade 1/questao6.php
@@ -0,0 +1,10 @@
+<?php
+
+//function da saudação
+function saudacao($nome) {
+    echo "Olá, " . $nome . "!";
+
+}
+
+//usa a função e printa o nome "Carl Johnson"
+saudacao("Carl Johnson da Groove Street");
