e13b565aae414ddfc62701cc08919718a1e4abec
Anchova <anchovaluiz@gmail.com>
2026-07-30 18:10:11 -0300

    commit da questão 4
    tabuada

---
+++ Atividade 1/questao5.php
@@ -0,0 +1,33 @@
+<?php
+
+
+//tabuada do 4
+echo "\n\n--- Tabuada do 4 ---";
+
+for ($i = 1; $i <= 10; $i++) {
+
+
+    echo "\n4 x " . $i . " = " . (4 * $i);
+}
+
+
+
+//Tabuada do 7
+echo "\n\n--- Tabuada do 7 ---";
+
+for ($i = 1; $i <= 10; $i++) {
+
+
+    echo "\n7 x " . $i . " = " . (7 * $i);
+}
+
+
+
+//Tabuada do 12879.5
+echo "\n\n--- Tabuada do 12879.5 ---";
+
+for ($i = 1; $i <= 10; $i++) {
+
+
+    echo "\n12879.5 x " . $i . " = " . (12879.5 * $i);
+}
