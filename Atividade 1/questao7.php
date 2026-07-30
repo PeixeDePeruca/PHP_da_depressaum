0d5498a09ebe68d2c37bb6846bab180e311f9511
Anchova <anchovaluiz@gmail.com>
2026-07-30 18:53:43 -0300

    questão 7

---
+++ Atividade 1/questao7.php
@@ -0,0 +1,14 @@
+<?php
+
+//function do input dos 2 números
+function calcular($numero1, $numero2) {
+
+    echo "\nSoma: " . ($numero1 + $numero2);
+    echo "\nSubtração: " . ($numero1 - $numero2);
+
+
+    echo "\nFrase incrível: Quem mora em Cubatão é o que?";
+}
+
+//chamando a function e passando os 2 números
+calcular(10, 5);
