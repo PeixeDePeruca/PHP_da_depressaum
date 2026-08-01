18d8581ec05fb1714599d5693f42a7e298d5f64d
Anchova <anchovaluiz@gmail.com>
2026-07-31 22:10:15 -0300

    questão 9 da atividade 1

---
+++ Atividade 1/questao9.php
@@ -0,0 +1,23 @@
+<?php
+
+
+//criação do array de raças favoritas de papagaio
+$racas_papagaio = [
+    "Louro-José 'a lenda'",
+    "Zé Carioca",
+    "Chatot",
+    "Squawkabilly",         
+    "Duolingo 'roubo na lista, ele é uma coruja'"
+];
+
+//adicionando +1 raça usando $array[] =
+//dentro dos colchetes irá se tornar o novo array, n modificar...interessante
+//ah, tmb n colocar ANTES dda lista pq se n vai printar a lista e n vai printar esse "papagaio africano sozinho"
+$racas_papagaio[] = "Papagaio Branco Africano";
+
+
+//"Papagaio Branco Africano"
+//resultado do cóigo, n remover
+print_r($racas_papagaio);
+
+?>  
